f37be9078b7601178d8e07f1523bae0d00fea380
RyanStyron <ryanstyron1220@gmail.com>
2024-08-28 20:08:53 -0400

    Add SimpleMovingAverage class for calculating SMA technical indicator.

---
+++ fx4sight/quant/indicators/simple_moving_average.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class SimpleMovingAverage():
+    def __init__(self, period: int = 14) -> None:
+        '''
+        The average of the closing prices over a specified number of periods.
+        The SMA is a lagging indicator that smooths price data to identify
+        trends. When a shorter-term SMA crosses above a longer-term SMA, it
+        is generally considered a bullish signal (golden cross). 
+        Conversely, when a shorter-term SMA crosses below a longer-term SMA, 
+        it is generally considered a bearish signal (death cross).
+        
+        Args:
+            period (int): Number of periods (recent price bars) to consider
+                when calculating the SMA. Defaults to 14.
+        '''
+        self.period = period
+        self.prices = []
+        self.sma = 0.0
+
+    def update(self, price: float) -> float:
+        '''
+        Updates the SMA with the latest price.
+        Ensure that the price's timestamp is
+        of the same granularity as the period.
+        
+        Args:
+            price (float): Latest price.
+        '''
+        self.prices.append(price)
+        
+        if len(self.prices) > self.period:
+            self.sma = np.mean(self.prices[-self.period:])
+        return self.sma
+    
+    def get(self) -> float:
+        '''
+        Returns the current SMA value (float).
+
+        If not updated with at least `period` prices, 
+        the SMA will be 0.0.
+        '''
+        return self.sma
