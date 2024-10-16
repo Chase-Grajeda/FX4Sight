02c2faff2075f14fc6ecf35353fe89b3f2851092
RyanStyron <ryanstyron1220@gmail.com>
2024-10-15 20:59:19 -0400

    Fix comment in quant package initializer.

--- fx4sight/quant/__init__.py
+++ fx4sight/quant/__init__.py
@@ -3,5 +3,5 @@ from .indicators.simple_moving_average import SimpleMovingAverage as SMA
 from .indicators.exponential_moving_average import ExponentialMovingAverage as EMA
 from .indicators.relative_strength_index import RelativeStrengthIndex as RSI
 
-# Classes to be imported when using `from fx4sight.quant import *`.
+# Classes to be imported when quant is packaged.
 __all__ = ["HammerSignal", "SMA", "EMA", "RSI"]
